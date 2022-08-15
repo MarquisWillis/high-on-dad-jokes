0db58edba77dec97c7f0c1f5934fe33f8f950130
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-08-14 20:15:31 -0400

    started main.handlebars file and added in bulma

---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment: {
+            type: DataTypes.TEXT,
+        },
+        date_created: {
+            type: DataTypes.STRING,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        dad_joke_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'dad-joke',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
