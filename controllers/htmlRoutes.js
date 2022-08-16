db7d2de2c7b7201745742eee9f55d11b6fa7961c
Marquis Willis <marquiswillis99@gmail.com>
2022-08-15 21:31:05 -0400

    added html routes for getting pages

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -4,13 +4,91 @@ const { User, DadJoke, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-// route for getting homepage
+// route for getting homepage ;; DONE (maybe)
 router.get('/', async (req, res) => {
     try {
+       res.render('homepage') 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// route for getting login/signup page ;; DONE
+router.get('/login', async (req, res) => {
+    try {
+        if(req.session.logged_in) {
+            res.redirect('/');
+            return;
+        }
+
+        res.render('login')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// route for getting all dad jokes page ;; DONE
+router.get('/jokes', async (req, res) => {
+    try {
+        const dadJokeData = await DadJoke.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
+
+        const dadJokes = dadJokeData.map((dadJoke) => dadJoke.get({plain : true}));
+
+        res.render('jokepage', {
+            ...dadJokes,
+            logged_in: req.session.logged_in
+        });
 
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// route for getting login
+// route for getting a single dad joke ;; DONE
+router.get('/jokes/:id', withAuth, async (req, res) => {
+    try {
+        const dadJokeData = await DadJoke.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+                {
+                    model: Comment
+                }
+            ]
+        });
+
+        const dadJoke = dadJokeData.get({ plain: true });
+
+        res.render('jokepage', {
+            ...dadJoke,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// route for getting create dad jokes page ;; DONE
+router.get('/create', withAuth, async (req, res) => {
+    try {
+        if (!req.session.logged_in) {
+            res.redirect('/login');
+            return;
+        }
+
+        res.render('add-joke');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
