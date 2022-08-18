388ad56f622acaf72aa712d97ff3fe78911a6146
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-08-17 20:29:51 -0400

    updated commentRoutes.js file

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth')
 
 // route for creating comment (include withAuth)
+router.post('/',withAuth , async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id:req.body.user_id,
+        });
+
+        res.status(201).json(newComment)
+    }
+    catch (err) {
+        res.status(500).json(err):
+    }
+})
 
 module.exports = router;
