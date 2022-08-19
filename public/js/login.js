2af8be56530893914166f2716e8aeeed2125e2d2
tgallagher496 <tgallagher496@gmail.com>
2022-08-19 19:23:51 -0400

    started login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,52 @@
 const loginFormHandler = async function (e) {
     e.preventDefault();
 
-    const emailInput = document.querySelector('#email');
-    const passwordInput = document.querySelector('#password');
+    const emailInput = document.querySelector('#email').ariaValueMax.trim();
+    const passwordInput = document.querySelector('#password').ariaValueMax.trim();
 
-    const response = await fetch('/')
+    if(emailInput&&passwordInput){
+        const response = await fetch('/login',{
+            method: 'POST',
+            body: JSON.stringify({emailInput, passwordInput}),
+            headers: {'Content-Type': 'application/json'},
+
+        });
+
+        if(response.ok){
+            document.location.replace('/jokes');
+        } else{
+alert(response.statusText);
+        }
+    }
+};
+
+const signupForm = async (event) => {
+event.preventDefault();
+
+const name = docuemnt.querySelector(`#name`).value.trim();
+const email = document.querySelector(`#email`).value.trim();
+const password = document.querySelector(`#password`).value.trim();
+
+if (name && email && password){
+    const response = await fetch(`/login`,{
+        method: 'POST',
+        body: JSON.stringify({name,email,password}),
+        headers: {'Content-Type': 'application/json'},
+    });
+
+    if (response.ok){
+        document.location.replace('/jokes');
+    }else{
+        alert(response.statusTEXT);
+    }
 }
+
+};
+
+document
+    .querySelector('.login')
+    .addEventListener('submit', loginFormHandler);
+
+document 
+    .querySelector('.jokes')
+    .addEventListener('submit', signupForm);
